db4ddac45a254fe54737344eade6ad79d1c82156
Harrison Zhao <roarofpie@gmail.com>
2014-05-30 23:01:09 -0400

    updated configs

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var configs = require('config/index');
 configs.configure(app);
 var path = require('path');
 
-app.set('views', path.join(__dirname, "views"));
-app.use(express.static('public'));
 //mount on on other modules
 //app.use('path', require('moduleName'))
 //keep this file clean
