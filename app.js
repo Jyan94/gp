91d40f09b7a972465df5bf773a7762d335f1f1f6
davidlu1995 <davidl1@andrew.cmu.edu>
2014-06-02 10:22:45 -0400

    update

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var configs = require('config/index');
 configs.configure(app);
 
 //mount on on other modules
-//app.use('path', require('moduleName'))
 //keep this file clean
 app.use('/', require('routes/index'));
 
