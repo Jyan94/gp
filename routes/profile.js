d39cc49dd1f2740a0bd146ae1f9cd5cca55d0552
jmiooo <leet2013@gmail.com>
2014-06-03 18:46:15 -0400

    Wow

--- routes/profile.js
+++ routes/profile.js
@@ -86,83 +86,100 @@ function retrieveProfile(req, res) {
  */
 
 function updateProfile(req, res) {
-  var upload_file = null;
-  var upload_filename = null;
-  var upload_user_id = null;
-
-  async.waterfall ([
-    function (callback) {
-      req.pipe(req.busboy);
-      req.busboy.on('file', function(fieldname, file, filename, encoding, 
+  /*req.busboy.on('file', function(fieldname, file, filename, encoding, 
                                      mimetype) {
         // Check that the user posted an appropriate photo
         if (mimetype !== 'image/gif' && mimetype !== 'image/jpeg' &&
             mimetype !== 'image/png') {
         // Again, clobber fraudulent requests
-          res.end();
+          res.send(500, 'Not a valid image file.');
         }
         else {
-          upload_file = file;
-          upload_filename = filename;
+          User.select('username', req.params.username, function (err, result) {
+            file.pipe(fs.createWriteStream(__dirname + '/' + filename));
+            User.update(result.user_id, ['image'], ['http://localhost:3000/images/' + filename], function (err, result) {
+              res.send('http://localhost:3000/images/' + filename);
+            });
+          });
+        }
+  });
+  
+  req.pipe(req.busboy);
+}*/
+  
+  var upload_file = null;
+  var upload_filename = null;
+  var upload_user_id = null;
+
+  req.busboy.on('file', function(fieldname, file, filename, encoding, 
+                                     mimetype) {
+    async.waterfall([
+      function (callback) {
+          console.log(2341);
+          // Check that the user posted an appropriate photo
+          if (mimetype !== 'image/gif' && mimetype !== 'image/jpeg' &&
+              mimetype !== 'image/png') {
+          // Again, clobber fraudulent requests
+            res.send(500, 'Not a valid file.');
+          }
+          else {
+            upload_file = file;
+            upload_filename = filename;
+            console.log(2341);
+            callback(null);
+          }
+        },
+      function (callback) {
+        User.select('username', req.params.username, function (err, result) {
+          if (err) {
+            res.send(500, 'Database error.');
+          }
+          else if (result) {
+            upload_user_id = result.user_id;
+            callback(null, result.image);
+          }
+          else {
+            res.send(404, messages.incorrect_username);
+          }
+        });
+      },
+      function (image, callback) {
+        if (image && (1===0)) {
+          fs.unlink(image, function (err) {
+            if (err) {
+              res.send(messages.delete_error);
+            }
+
+            upload_file.pipe(fs.createWriteStream(__dirname + '/' + upload_filename));
+            callback(null);
+          });
+        }
+        else {
+          upload_file.pipe(fs.createWriteStream(__dirname + '/' + upload_filename));
           callback(null);
         }
-      });
-    },
-    function (callback) {
-      User.select('username', req.params.username, function (err, result) {
-        if (err) {
-          console.log(err);
-        }
-        else if (result) {
-          upload_user_id = result.user_id;
-          callback(null, result.image);
-        }
-        else {
-          res.send(messages.incorrect_username);
-        }
-      });
-    },
-    function (image, callback) {
-      if (image && (1===0)) {
-        fs.unlink(image, function (err) {
+      },
+      function (callback) {
+        User.update(upload_user_id, ['image'], ['/images/' + upload_filename], function (err, result) {
           if (err) {
-            res.send(messages.delete_error);
+            res.send(500, 'Database error.');
           }
-          
-            image = 'file://' + __dirname + '/' + upload_filename;
-            callback(null, image);
+          else {
+            res.send('/images/' + upload_filename);
+            callback(null);
+          }
         });
       }
-      else {
-        image = 'file://' + __dirname + '/' + upload_filename;
-        console.log(image);
-        callback(null, image);
+    ], function (err) {
+      if (err) {
+        console.log(err);
       }
-    },
-    function (image, callback) {
-      //upload_file.pipe(fs.createWriteStream(image));
-      callback(null, image);
-    },
-    function (image, callback) {
-      console.log(image);
-      User.update(upload_user_id, ['image'], [image], function (err, result) {
-        if (err) {
-          res.send(messages.update_error);
-        }
-        else {
-          callback(null, result);
-        }
-      });
-    }
-  ], function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      return { image: upload_filename };
-    }
+    });
   });
+
+  req.pipe(req.busboy);
 }
+
 
 app.route('/user')
 .get(function (req, res) {
@@ -172,5 +189,13 @@ app.route('/user')
 
 app.route('/user/:username').get(retrieveProfile);
 app.route('/upload/image/:username').post(updateProfile);
+
+
+app.route('/images/:file').get(function (req, res) {
+  file = req.params.file;
+  var img = fs.readFileSync(__dirname + '/' + file);
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
 
 app.listen(3000);
