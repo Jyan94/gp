652162a1b65e2b0a2f02dee2038d1a8138f513d6
Harrison Zhao <roarofpie@gmail.com>
2014-06-10 17:56:11 -0400

    added static pages

---
+++ routes/static/routes.js
@@ -0,0 +1,53 @@
+exports.about = function (req, res) {
+  if (req.user) {
+    res.render('logout/about.html');
+  }
+  else {
+    res.render('login/about.html');
+  }
+}
+
+exports.contact = function (req, res) {
+  if (req.user) {
+    res.render('logout/contact.html');
+  }
+  else {
+    res.render('login/contact.html');
+  }
+}
+
+exports.faq = function (req, res) {
+  if (req.user) {
+    res.render('logout/faq.html');
+  }
+  else {
+    res.render('login/faq.html');
+  }
+}
+
+exports.features = function (req, res) {
+  if (req.user) {
+    res.render('logout/faq.html');
+  }
+  else {
+    res.render('login/faq.html');
+  }
+}
+
+exports.home = function (req, res) {
+  if (req.user) {
+    res.render('logout/home.html');
+  }
+  else {
+    res.render('login/home.html');
+  }
+}
+
+exports.rules = function (req, res) {
+  if (req.user) {
+    res.render('logout/rules.html');
+  }
+  else {
+    res.render('login/rules.html');
+  }
+}
