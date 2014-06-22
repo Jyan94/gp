16a0883d6dec10fd9116bcfa33d954ab755a7122
jmiooo <leet2013@gmail.com>
2014-06-22 14:24:42 -0400

    Dohoho

--- routes/registry/signup.js
+++ routes/registry/signup.js
@@ -35,7 +35,7 @@ function insertUser(body, res, next) {
       null, //age
       null, //address
       null, //payment_info
-      {value: 0.0, hint: 'double'}, //money
+      {value: 10000.0, hint: 'double'}, //money
       null, //fbid
       0,  //vip_status
       null //image
