ecd060df5166c02716d0345d04d2fb592ea6645f
Jeffrey <jeffreyyan8@gmail.com>
2014-07-16 23:09:04 -0700

    stuff

--- scripts/baseball/parseheadshotImagesMlb.py
+++ scripts/baseball/parseheadshotImagesMlb.py
@@ -1,6 +1,7 @@
 from cassandra.cluster import Cluster
 import json
 import uuid
+import urllib
 
 cluster = Cluster(['localhost'])
 session = cluster.connect('goprophet')
@@ -15,9 +16,10 @@ print length
 for num in range(0, length):
   title = data["assetlist"]["asset"][num]["title"][1: -1]
   print title
-  #playerId = data["assetlist"]["asset"][num]["@player_id"]
+  playerId = data["assetlist"]["asset"][num]["@player_id"]
   href = data["assetlist"]["asset"][num]["links"]["link"][0]["@href"]
   url = 'http://api.sportsdatallc.org/mlb-images-t2/usat' + href + '?api_key=9drv6ypfuenurvjw9z52fba6'
+  #urllib.urlretrieve(url, '/Users/Owner2/Documents/GPbaseball/' + playerId + '.jpg')
   session.execute(
         """
         INSERT INTO player_images (player_name, image_url)
