0e24ca1c7c1080bbf4be0b40e413bc1625b31486
jmiooo <leet2013@gmail.com>
2014-07-21 15:37:13 -0400

    Can edit instances for Contest B.

--- public/js/contestBCreation.js
+++ public/js/contestBCreation.js
@@ -14,4 +14,29 @@ $(document).ready(function() {
 
     gameBox.toggleClass('active');
   });
-})
+
+  var maximumEntriesFiftyFifty = '<input type="number" name="maximum-entries" placeholder="Maximum Entries" min="3" max="2001" required="true" step="2" value ="11" id="maximum-entries">\n';
+  var maximumEntriesNormal = '<select name="maximum-entries" id="maximum-entries">\n'
+                             + '  <option value="2">2</option>\n'
+                             + '  <option value="3">3</option>\n'
+                             + '  <option value="5">5</option>\n'
+                             + '  <option value="10">10</option>\n'
+                             + '  <option value="12">12</option>\n'
+                             + '  <option value="14">14</option>\n'
+                             + '  <option value="23">23</option>\n'
+                             + '  <option value="56">56</option>\n'
+                             + '  <option value="112">112</option>\n'
+                             + '  <option value="167">167</option>\n'
+                             + '  <option value="230">230</option>\n'
+                             + '  <option value="1150">1150</option>\n'
+                             + '</select>\n'; 
+
+  $(document).on('change', '#is-fifty-fifty', function (e) {
+    if ($('#is-fifty-fifty').val() === 'true') {
+      $('#maximum-entries').replaceWith(maximumEntriesFiftyFifty);
+    }
+    else {
+      $('#maximum-entries').replaceWith(maximumEntriesNormal);
+    }
+  });
+});
--- views/handlebarHelpers/helpers.js
+++ views/handlebarHelpers/helpers.js
@@ -38,3 +38,36 @@ exports.contestBCreationHelperHome = function (context, options) {
 
   return ret;
 };
+
+exports.contestBEntryHelper = function (context, options) {
+  var athletes = context.athletes;
+  var ret = "";
+
+  for (var i = 0; i < athletes.length; i++) {
+    ret += options.fn({
+                        athlete: athletes[i],
+                        index: i,
+                        maxWager: context.maxWager
+                      });
+  }
+
+  return ret;
+}
+
+exports.contestBEditHelper = function (context, options) {
+  var athletes = context.athletes;
+  var contestantInstance = context.contestantInstance;
+  var ret = "";
+
+  for (var i = 0; i < athletes.length; i++) {
+    ret += options.fn({
+                        athlete: athletes[i],
+                        index: i,
+                        prediction: contestantInstance.predictions[i],
+                        wager: contestantInstance.wagers[i],
+                        maxWager: context.maxWager
+                      });
+  }
+
+  return ret;
+};
