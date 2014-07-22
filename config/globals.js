f0275bbf2da621e71c7f16b80ea3ff9a232a73ac
Harrison Zhao <roarofpie@gmail.com>
2014-07-21 22:32:41 -0700

    update

--- config/globals.js
+++ config/globals.js
@@ -2,8 +2,71 @@
  * contains cached bets
  * @type {Object}
  */
+/*
+generic bet:
+{
+  athleteId uuid, --
+  athleteName text, --
+  athleteTeam text, --
+  betId timeuuid, --
+  betterUsernames list<text> ??,    
+  expirations list<timestamp>,  ??  
+  fantasyValue double, --
+  gameId uuid, --
+  isSellingPosition list<boolean>, ??
+  oldPrices,
+  payoff double, --
+  prices list<double>, ??
+  sport text --
+}
+
+pending bet format:
+{
+  athleteId uuid, --
+  athleteName text, --
+  athleteTeam text, --
+  betId timeuuid, --
+
+  better (text)
+  expiration (timestamp formatted as milliseconds since epoch)
+  overNotUnder (boolean specifying over or under bet position available)
+  price (double)
+
+  fantasyValue double, --
+  gameId uuid, --
+  payoff double --
+}
+
+resell bet format:
+{
+  athleteId uuid, --
+  athleteName text, --
+  athleteTeam text, --
+  betId timeuuid, --
+
+  isSellingPosition - array of length 2 contains 2 booleans
+  betterUsernames - array of length 2 contains users for respective positions
+  expirations - array of length 2 contains expirations for bets
+  prices - array of length 2 contains prices for resell positions
+
+  fantasyValue double, --
+  gameId uuid, --
+  payoff double --
+}
+
+TO DO taken bet formatting
+ */
 exports.contestA = {
-  pendingBets: [{athleteId: '10154eef-8834-48e0-97e7-d7436367534c', athleteName: 'Adrian Gonzalez', athleteTeam: 'LA',seller: 'hello world'}],
+  pendingBets: [
+    {
+      athleteId: '10154eef-8834-48e0-97e7-d7436367534c', 
+      athleteName: 'Adrian Gonzalez', 
+      athleteTeam: 'LA',
+      better: 'hello world',
+      overNotUnder: true,
+
+    }
+    ],
   resellBets: [],
   takenBets: []
 }
---
+++ libs/contestA/formatBets.js
@@ -0,0 +1,54 @@
+'use strict';
+require('rootpath')();
+
+var configs = require('configs/index');
+var positions = configs.constants.contestA.POSITIONS;
+
+var async = require('async');
+
+var contestAGlobals = configs.globals.contestA;
+var pendingBets = contestAGlobals.pendingBets;
+var resellBets = contestAGlobals.resellBets;
+var takenBets = contestAGlobals.takenBets;
+var OVER = positions.OVER;
+var UNDER = positions.UNDER;
+
+function getUserPending(username, callback) {
+  async.filter(pendingBets, function(bet, callback) {
+    callback(bet.better === username);
+  }, callback);
+}
+
+function getUserResell(username, callback) {
+  async.filter(resellBets, function(bet, callback) {
+    callback(
+      (bet.isSellingPosition[OVER] && 
+        bet.betterUsernames[OVER] === username) ||
+      (bet.isSellingPosition[UNDER] && 
+        bet.betterUsernames[UNDER] === username));
+  }, callback);
+}
+
+function getUserTaken(username, callback) {
+  async.filter(takenBets, function(bet, callback) {
+    callback(
+      (bet.isSellingPosition[OVER] && 
+        bet.betterUsernames[OVER] === username) ||
+      (bet.isSellingPosition[UNDER] && 
+        bet.betterUsernames[UNDER] === username));
+  }, callback);
+}
+
+function getPrimaryMarket(username, callback) {
+  async.filter(pendingBets, function(bet, callback) {
+    callback(bet.better !== username);
+  }, callback);
+}
+
+function getSecondaryMarket(username, callback) {
+  async.filter(resellBets, function(bet, callback) {    
+    callback(
+      bet.betterUsernames[OVER] !== username && 
+      bet.betterUsernames[UNDER] !== username);
+  }, callback);
+}
