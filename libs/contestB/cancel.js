03ae8feb13f6522abb52d8c188b82cfd08221f69
Harrison Zhao <roarofpie@gmail.com>
2014-07-24 16:31:33 -0700

    update

--- libs/contestB/cancel.js
+++ libs/contestB/cancel.js
@@ -9,6 +9,14 @@
 var async = require('async');
 var User = require('libs/cassandra/user');
 
+/**
+ * adds money back to user for a cancelled contest
+ * whose contestants map not been JSON.parsed yet
+ * @param  {object}   contest
+ * read from database
+ * @param  {Function} callback 
+ * args: (err)
+ */
 function refundCancelledContestUsers(contest, callback) {
   async.each(Object.keys(contest.contestants), function(key, callback) {
     if (contest.contestants.hasOwnProperty(key)) {
