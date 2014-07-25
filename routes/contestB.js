ff35fe8534b1e046bfaf4cf78f1ef247a9ce2ee3
jmiooo <leet2013@gmail.com>
2014-07-24 20:23:36 -0400

    Contest B done for now.

--- routes/contestB.js
+++ routes/contestB.js
@@ -942,8 +942,8 @@ var updateStateContestsOpenAndFilledHelper = function (currentTime) {
     var finalCallback = function (err) {
       callback(err);
     }
-
-    if (contest.contest_deadline_time + 900000 <= currentTime) {
+    
+    if (contest.contest_deadline_time.getTime() + 900000 <= currentTime.getTime()) {
       if (contest.current_entries >= contest.minimum_entries) {
         ContestB.setToProcess(contest.contest_id, finalCallback);
       }
