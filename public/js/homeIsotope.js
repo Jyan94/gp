ad24fd5f9e778c80aef6e4a9892c4da2e0172761
Jeffrey <jeffreyyan8@gmail.com>
2014-07-28 17:57:58 -0700

    filter part 1

--- public/js/homeIsotope.js
+++ public/js/homeIsotope.js
@@ -16,20 +16,75 @@ function debounce( fn, threshold ) {
 $( function() {
   // quick search regex
   var qsRegex;
-
+  var buttonFilter;
+  var overUnderFilter;
   // init Isotope
   var $container = $('.isotope').isotope({
     itemSelector: '.playercard1',
     layoutMode: 'fitRows',
     filter: function() {
-      return qsRegex ? $(this).text().match( qsRegex ) : true;
+      var $this = $(this);
+      var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+      var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+      var overUnderResult = overUnderFilter ? $this.is(overUnderFilter) : true;
+      return searchResult && buttonResult;
     }
+    /*getSortData: {
+      nfl: '.Football',
+      nba: '.Basketball',
+      mlb: '.Baseball',
+    }*/
+  });
+
+
+    // filter functions
+  /*var filterFns = {
+    wager: function(wageBottom, wageTop) {
+      var wager = $(this).find('.playercard1-bottom.wager').text().split(" ")[0];
+      //parseFlaot vs parseDouble??
+      return (parseFloat(wager) > wageBottom
+              && parseFloat(wager) < wageTop);
+    },
+    over: function() {
+      var overOrUnder = $(this).find('.playercard1-bottom.wager').text().split(" ")[1];
+      return overOrUnder.match(/over/);
+    },
+    under: function() {
+      var overOrUnder = $(this).find('.playercard1-bottom.wager').text().split(" ")[1];
+      return overOrUnder.match(/under/);
+    },
+    fantasyValue: function(fantasyValueBottom, fantasyValueTop) {
+      var fantasyValue = $(this).find('.playercard1-bottom.wager').text().split(" ")[2];
+      return (parseFloat(fantasyValue) > fantasyValueBottom
+              && parseFloat(fantasyValue) < fantasyValueTop);
+    }
+  };
+*/
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var $this = $(this);
+    buttonFilter = $this.attr('data-filter');
+    // use filterFn if matches value
+    var $buttonGroup = $this.parents('.button-group');
+    //var overUnderValue = $buttonGroup.attr('data-filter-group');
+    /*var overUnderValue = $this.attr('data-filter');
+    overUnderFilter = filterFns[ overUnderValue ] || overUnderValue;*/
+    $container.isotope();
   });
 
   // use value of search field to filter
   var $quicksearch = $('#quicksearch').keyup( debounce( function() {
     qsRegex = new RegExp( $quicksearch.val(), 'gi' );
     $container.isotope();
-  }, 200 ) );
+  }) );
+
+    // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
 
 });
